4a23673c570550a12d1513a08c4ebc77a331929e
morning-start <morning-start@foxmail.com>
2025-06-28 19:24:31 +0800

    build: 更新 auto-pr 脚本的上游仓库地址
    - 将上游仓库地址从 "<username>/<bucketname>:main" 修改为 "morning-start/code-bucket:main"
    - 这个修改确保了 auto-pr 脚本能够正确地与指定的上游仓库进行交互

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "morning-start/code-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
